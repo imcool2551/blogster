6c5ee62d299ae49b2fe007a45cc6a43ed6fa3585
SangWook <imcool2551@gmail.com>
2021-05-21 04:37:13 +0900

    added deploy configuration

--- client/src/apis/api.js
+++ client/src/apis/api.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: 'http://localhost:5000/',
+  // baseURL: 'http://localhost:5000/',
+  baseURL: 'https://api.wsangwook.com/',
   headers: {
     Authorization: {
       toString() {
